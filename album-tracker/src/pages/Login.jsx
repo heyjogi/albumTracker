e0ab7a7112c3f3095288fe0c6954f77d39b07601
heyjogi <hj1021007@gmail.com>
2026-04-02 11:57:01 +0900

    logo.svg 수정

--- album-tracker/src/pages/Login.jsx
+++ album-tracker/src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
         <div className="login-wrap">
             <div className="login-card">
                 <div className="login-logo">
-                    <img src="/logo.svg" alt="Album Tracker Logo" className="w-full h-full rounded-xl" />
+                    <img src="/logo.png" alt="Album Tracker Logo" className="w-full h-full rounded-xl" />
                 </div>
                 <h1 className="login-title">Album Tracker</h1>
                 <p className="login-desc">앨범 분철 정산을 간편하게 관리하세요.</p>
